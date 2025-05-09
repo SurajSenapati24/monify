c684393a0eb0b6dbe01dee527038b079b293e901
Suraj Senapati <surajsenapati58@gmail.com>
2025-05-09 22:35:12 +0530

    monify basic code

---
+++ components/header.jsx
@@ -0,0 +1,15 @@
+import { SignedIn,SignedOut,SignInButton,UserButton,SignUpButton } from "@clerk/nextjs"
+const Header = () => {
+  return (
+    <div>
+       <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+    </div>
+  )
+}
+export default Header
